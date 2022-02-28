9e7a08b5c46d0b8c8d14fe86e16ddb8d5f14d2dd
Alicia <arrowsrp@yahoo.com>
2022-02-28 11:23:44 -0800

    commit tree 87231b9426af2e8301d3bf5d7840afa122df8cf1

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const[itemName, setItemName]= useState("");
+  const [itemCategory, setItemCategory]= useState('Produce');
+
+  function onNameChange(event){
+    setItemName(event.target.value);
+    console.log(itemName)
+  }
+
+  function onCategoryChange(event){
+    setItemCategory(event.target.value);
+    console.log(itemCategory);
+  }
+  
+  const newItem = {
+    id: uuid(),
+    name: itemName,
+    category: itemCategory,
+  }
+
+  function onSubmit (event){
+    event.preventDefault();
+    onItemFormSubmit(newItem);
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={onNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={onCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
